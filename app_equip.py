c7cf8df7ac6ea0fc9a5dda6617c940425f41e540
Donchack <flexway7@gmail.com>
2022-03-17 18:04:02 +0300

    Made an addition to the cost of equipment

--- app_equip.py
+++ app_equip.py
@@ -11,7 +11,8 @@ def eqip_prices() -> str:
     s_name_eq = request.form.get('search_name_eq','')
     s_type_eq = request.form.get('search_type_eq','')
     
-    result = dbs.get_eqips(s_id, s_name_eq, s_type_eq)
+    result = dbs.get_eqips(id = s_id, name_eq = s_name_eq, type_eq = s_type_eq)
+    
     # result = {1: ('Трансформатор ТОЛ-10 III-2-0,5S/10Р-30/5 УХЛ1','Трансформатор'), 2: ('Трансформатор ТОЛ-10-III-2-0,5S/10P 75/5 УХЛ1','Трансформатор'), 3: ('Счетчик МИР С-03.02Т-ЕQTLBMN-RG-1T-H','Счетчик')}
     return render_template('eqip_prices.html', the_title='CПИСОК ОБОРУДОВАНИЯ', the_results=result)
 
@@ -32,9 +33,30 @@ def del_eqip():
     dbs = db_sqlite.EquipDB()
     if request.form.get('ID_equip'):
         dbs.del_equip(request.form['ID_equip'])
-        
     result = dbs.get_eqips()
     return render_template('del_equip_form.html', the_title='УДАЛЕНИЕ ОБОРУДОВАНИЯ', the_results=result)
+
+@app.route('/1_equip', methods=['GET','POST'])
+def one_eqip() -> str:
+    dbs = db_sqlite.EquipDB()
+    filter_id = request.form.get('id_1_eq','')
+    res_equip = dbs.get_eqips(filter_id, '', '')
+    equip = next(res_equip)
+    res_prices = dbs.get_prices(equip[0])
+    return render_template('1_equip.html', the_title='Одна позиция', the_res1=equip, the_res2=res_prices)
+
+@app.route('/add_price', methods=['POST'])
+def add_price() -> str:
+    dbs = db_sqlite.EquipDB()
+    filter_id = request.form.get('id_1_eq','')
+    # берем первое значение возвращаемое итерируемым объектом типа cursor SQlite
+    # если результат выполнения функции get_eqips сначала присвоить отдельной переменной 
+    # и взять первое значение этой переменной,
+    # то при выполнении следующей функции с добавлением данных в таблицу возникнет блокировка БД
+    equip = next(dbs.get_eqips(filter_id, '', ''))
+    dbs.add_price(filter_id,request.form['price'], request.form['date'], request.form['actual'])
+    res_prices = dbs.get_prices(equip[0])
+    return render_template('1_equip.html', the_title='Одна позиция', the_res1=equip, the_res2=res_prices)    
 
 if __name__ == '__main__':
     app.run(debug=True)
--- db_sqlite.py
+++ db_sqlite.py
@@ -1,35 +1,50 @@
 import sqlite3
+from time import sleep
 
 class EquipDB:
     def __init__(self, database: str = 'equip.sqlite'):
         self._database = database
         with sqlite3.connect(self._database) as connection:
-            cursor = connection.cursor()
-            cursor.execute("""create table if not exists equips (
+            connection.execute("""create table if not exists equips (
                             id integer not null primary key autoincrement,
                             equip text not null,
                             type text);""")
-            connection.commit()
+            connection.execute("""create table if not exists prices (
+                            id integer not null primary key autoincrement,
+                            id_eq integer not null,
+                            price real not null,
+                            date integer,
+                            actual integer, 
+                            foreign key (id_eq) references equips(id));""")                
+            
+            
     
-    def get_eqips(self, s_id="", s_name_eq="", s_type_eq=""):
+    def get_eqips(self, id="", name_eq="", type_eq=""):
         with sqlite3.connect(self._database) as connection:
-            cursor = connection.cursor()
-            result = cursor.execute("""select * from equips where id LIKE ? and equip LIKE ? and type LIKE ?""", (f'%{s_id}%',f'%{s_name_eq}%', f'%{s_type_eq}%'))
+            result = connection.execute("""select * from equips where id LIKE ? and equip LIKE ? and type LIKE ?""", (f'%{id}%',f'%{name_eq}%', f'%{type_eq}%'))
         return result
     
     def add_equip(self, equip, type):
         with sqlite3.connect(self._database) as connection:
-            cursor = connection.cursor()
-            cursor.execute("""insert into equips (equip, type)
+            connection.execute("""insert into equips (equip, type)
                             values(?, ?)""", (equip, type))
-            connection.commit()
     
     def del_equip(self, id):
         with sqlite3.connect(self._database) as connection:
-            cursor = connection.cursor()
-            cursor.execute("""delete from equips 
+            connection.execute("""delete from equips 
                             where id = ?""", (id, ))
-            connection.commit()
+
+    def get_prices(self, id_eq=""):
+        with sqlite3.connect(self._database) as connection:
+            result = connection.execute("""select * from prices where id_eq=?""", (id_eq,))
+        return result
+
+    def add_price(self, id_eq, price=0.0, date=0, actual=''):
+        with sqlite3.connect(self._database) as connection:
+            cursor = connection.cursor()
+            cursor.execute("""insert into prices (id_eq, price, date, actual)
+                            values(?, ?, ?, ?)""", (id_eq, price, date, actual))
+
 
 
     
